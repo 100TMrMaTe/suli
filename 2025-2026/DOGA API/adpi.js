5d4b4131597a2c629a0579e447be1256d0cb7ec1
100TMrMaTe <konyhasi.mate.21@ady-nagyatad.hu>
2025-12-04 12:27:27 +0100

    bla

---
+++ 2025-2026/DOGA API/adpi.js
@@ -0,0 +1,195 @@
+document.body.onload = init();
+
+function init() {
+    feladat4get();
+    feladat5get();
+}
+
+function feladat2() {
+    fetch("index.php", {
+        method: "POST",
+        body: JSON.stringify({
+            feladat: "2",
+        }),
+    })
+        .then((x) => x.json())
+        .then((valasz) => {
+            document.getElementById("feladat2text").innerHTML = "legelso tanc: " + valasz[0][0] + " legutolso tanc: " + valasz[0][1];
+        });
+}
+
+function feladat3() {
+    let tanc = document.getElementById("feladat3select").value
+
+    fetch("index.php", {
+        method: "POST",
+        body: JSON.stringify({
+            feladat: "3",
+            tanc: tanc,
+        }),
+    })
+        .then((x) => x.json())
+        .then((valasz) => {
+            //console.log(valasz);
+            document.getElementById("feladat3text").innerHTML = tanc + "-t " + valasz + "-an tancoltak"
+        });
+}
+
+function feladat4get() {
+    fetch("index.php")
+        .then(x => x.json())
+        .then(y => {
+            //console.log(y)
+            y.fiuk.forEach(element => {
+                //console.log(element)
+                fiu = element;
+                //lany = element[1];
+                //document.getElementById("feladat4selectlany").innerHTML += "<option>"+lany+"</option>"
+                document.getElementById("feladat4select").innerHTML += "<option>" + fiu + "</option>"
+            });
+
+            y.lanyok.forEach(element => {
+                //console.log(element)
+                lany = element;
+                //lany = element[1];
+                //document.getElementById("feladat4selectlany").innerHTML += "<option>"+lany+"</option>"
+                document.getElementById("feladat4select").innerHTML += "<option>" + lany + "</option>"
+            });
+        })
+}
+
+function feladat4() {
+    let ember = document.getElementById("feladat4select").value
+
+    fetch("index.php", {
+        method: "POST",
+        body: JSON.stringify({
+            feladat: "4",
+            ember: ember,
+        }),
+    })
+        .then((x) => x.json())
+        .then((valasz) => {
+            //console.log(valasz);
+            document.getElementById("feladat4text").innerHTML = ember + " ezeket a táncokat táncolta: "
+
+            valasz.forEach(x => {
+                document.getElementById("feladat4text").innerHTML += x + ", "
+            })
+        });
+}
+
+function feladat5get() {
+    fetch("index.php")
+        .then(x => x.json())
+        .then(y => {
+            //console.log(y)
+            y.fiuk.forEach(element => {
+                //console.log(element)
+                fiu = element;
+                //lany = element[1];
+                //document.getElementById("feladat4selectlany").innerHTML += "<option>"+lany+"</option>"
+                document.getElementById("feladat5select").innerHTML += "<option>" + fiu + "</option>"
+            });
+
+            y.lanyok.forEach(element => {
+                //console.log(element)
+                lany = element;
+                //lany = element[1];
+                //document.getElementById("feladat4selectlany").innerHTML += "<option>"+lany+"</option>"
+                document.getElementById("feladat5select").innerHTML += "<option>" + lany + "</option>"
+            });
+        })
+}
+
+function feladat5() {
+    let ember = document.getElementById("feladat5select").value
+    let tanc = document.getElementById("feladat5selecttanc").value
+
+    fetch("index.php", {
+        method: "POST",
+        body: JSON.stringify({
+            feladat: "5",
+            ember: ember,
+            tanc: tanc,
+        }),
+    })
+        .then((x) => x.json())
+        .then((valasz) => {
+            //console.log(valasz);
+
+            if (valasz.length == 0) {
+                document.getElementById("feladat5text").innerHTML = ember + " nem táncolt " + tanc + "-t";
+            } else {
+                document.getElementById("feladat5text").innerHTML = "A " + tanc + " bemutatóján " + ember + " párja " + valasz + " volt.";
+            }
+
+
+        });
+}
+
+function feladat6() {
+
+
+    fetch("index.php", {
+        method: "POST",
+        body: JSON.stringify({
+            feladat: "6",
+        }),
+    })
+        .then((x) => x.json())
+        .then((valasz) => {
+            //console.log(valasz);
+            document.getElementById("feladat6text").innerHTML = "legtöbször szerepelt fiu/fiuk: ";
+            valasz.fiuk.forEach(x => {
+                document.getElementById("feladat6text").innerHTML += x + ", ";
+            })
+
+            document.getElementById("feladat6text").innerHTML += "és " + valasz.lanyokszam + " produkciban vettek reszt. <br>";
+
+            document.getElementById("feladat6text").innerHTML += "legtöbször szerepelt lany/lanyok: ";
+            valasz.lanyok.forEach(x => {
+                document.getElementById("feladat6text").innerHTML += x + ", ";
+            })
+
+            document.getElementById("feladat6text").innerHTML += "és " + valasz.lanyokszam + " produkciban vettek reszt. <br>";
+        });
+}
+
+function feladat7() {
+
+    fetch("index.php", {
+        method: "POST",
+        body: JSON.stringify({
+            feladat: "7",
+        }),
+    })
+        .then(x => x.json())
+        .then(valasz => {
+
+            // Tánc neve
+            document.getElementById("feladat7text").innerHTML =
+                "Legtöbbször előadott tánc: " + valasz[0].tanc;
+
+            const table = document.getElementById("table");
+            table.classList.remove("d-none");
+
+            // fejléc újraírása
+            table.innerHTML = `
+                <tr>
+                    <th class="border-2">FIÚK</th>
+                    <th class="border-2">LÁNYOK</th>
+                </tr>
+            `;
+
+            // táblázat kitöltése
+            valasz.forEach(elem => {
+                table.innerHTML += `
+                    <tr>
+                        <td class="border-2">${elem.fiu}</td>
+                        <td class="border-2">${elem.lany}</td>
+                    </tr>
+                `;
+            });
+        });
+}
---
+++ 2025-2026/DOGA API/index.php
@@ -0,0 +1,156 @@
+<?php
+include_once "../PHP/fugvenyek.php";
+
+$myfile = fopen("tancrend.txt", "r") or die("Unable to open file!");
+// Output one character until end-of-file
+$szamolas = 0;
+$tomb = [];
+while (!feof($myfile)) {
+    $sor = trim(fgets($myfile));
+    $szamolas++;
+    $tomb[] = $sor;
+    if ($szamolas % 3 == 0) {
+        $adatok[] = [
+            "tanc" => $tomb[0],
+            "lany" => $tomb[1],
+            "fiu"  => $tomb[2],
+        ];
+        $tomb = [];
+    }
+}
+fclose($myfile);
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $json = file_get_contents("php://input");
+    $data = json_decode($json, true);
+
+    if ($data["feladat"] == "2") {
+        $megoldas[] = [
+            $adatok[0]["tanc"],
+            $adatok[count($adatok) - 1]["tanc"],
+        ];
+
+        $json = json_encode($megoldas);
+        echo $json;
+    } elseif ($data["feladat"] == "3") {
+
+        $megoldas = 0;
+        foreach ($adatok as $x) {
+            if ($x["tanc"] == $data["tanc"]) {
+                $megoldas++;
+            }
+        }
+
+        $json = json_encode($megoldas);
+        echo $json;
+    } elseif ($data["feladat"] == "4") {
+
+        $megoldas = [];
+        foreach ($adatok as $x) {
+            if ($x["lany"] == $data["ember"] || $x["fiu"] == $data["ember"]) {
+                $megoldas[] = $x["tanc"];
+            }
+        }
+
+        $json = json_encode($megoldas);
+        echo $json;
+    } elseif ($data["feladat"] == "5") {
+
+        $megoldas = [];
+        foreach ($adatok as $x) {
+            if ($x["lany"] == $data["ember"] && $x["tanc"] == $data["tanc"]) {
+                $megoldas[] = $x["fiu"];
+            } elseif ($x["fiu"] == $data["ember"] && $x["tanc"] == $data["tanc"]) {
+                $megoldas[] = $x["lany"];
+            }
+        }
+
+        $json = json_encode($megoldas);
+        echo $json;
+    } elseif ($data["feladat"] == "6") {
+        $fiukdb = [];
+        $lanyokdb = [];
+
+        foreach ($adatok as $x) {
+            if (!isset($fiukdb[$x["fiu"]])) {
+                $fiukdb[$x["fiu"]] = 0;
+            }
+            $fiukdb[$x["fiu"]]++;
+
+            // Lányok gyakorisága
+            if (!isset($lanyokdb[$x["lany"]])) {
+                $lanyokdb[$x["lany"]] = 0;
+            }
+            $lanyokdb[$x["lany"]]++;
+        }
+
+        $maxfiu = max($fiukdb);
+        $maxlany = max($lanyokdb);
+
+        $legtobbszorfiuk = array_keys($fiukdb, $maxfiu);
+        $legtobbszorlanyok = array_keys($lanyokdb, $maxlany);
+
+        $megoldas = [
+            "fiuk" => $legtobbszorfiuk,
+            "fiukszam" => $maxfiu,
+            "lanyok" => $legtobbszorlanyok,
+            "lanyokszam" => $maxlany
+        ];
+
+        $json = json_encode($megoldas);
+        echo $json;
+    } elseif ($data["feladat"] == "7") {
+        $tanc = [];
+
+        foreach ($adatok as $x) {
+            if (!isset($tanc[$x["tanc"]])) {
+                $tanc[$x["tanc"]] = 0;
+            }
+            $tanc[$x["tanc"]]++;
+        }
+        $maxtanc = max($tanc);
+        $legtobbtanc = array_keys($tanc, $maxtanc);
+
+
+        foreach ($adatok as $x) {
+            if ($x["tanc"] == $legtobbtanc[0]) {
+                $megoldas[] = [
+                    "tanc" => $x["tanc"],
+                    "fiu" => $x["fiu"],
+                    "lany" => $x["lany"]
+                ];
+            }
+        }
+
+
+        $json = json_encode($megoldas);
+        echo $json;
+    }
+} elseif ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+    $json = file_get_contents("php://input");
+    $data = json_decode($json, true);
+
+
+    $fiuk = [];
+    $lanyok = [];
+
+    foreach ($adatok as $x) {
+        if (!in_array($x["lany"], $lanyok)) {
+            $lanyok[] = $x["lany"];
+        }
+        if (!in_array($x["fiu"], $fiuk)) {
+            $fiuk[] = $x["fiu"];
+        }
+    }
+
+    $megoldas = [
+        "lanyok" => $lanyok,
+        "fiuk" => $fiuk
+    ];
+
+    $json = json_encode($megoldas);
+    echo $json;
+}
