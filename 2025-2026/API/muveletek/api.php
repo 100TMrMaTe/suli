2296ceca3158530502e467de35316363c0166e59
100TMrMaTe <konyhasi.mate.21@ady-nagyatad.hu>
2025-12-11 09:52:35 +0100

    updated

--- 2025-2026/API/muveletek/api.php
+++ 2025-2026/API/muveletek/api.php
@@ -1,33 +1,73 @@
 <?php
-include_once "../../PHP/fugvenyek.php";
-//phpinfo();
+
 /*
-        osszeadas
-            url: osszead
-            method: POST
-        kivonas
-            url: kivonas
-            method: POST
-        szorzas
-            url: szorzas
-            method: POST
-        osztas
-            url: osztas
-            method: POST
-    */
+összeadás
+    min 2 szám összeadása
+    csak 1 szám: hiba: min 2 szám kell
+    ha sok szám: adja össze mind
+    ha nem szám valamelyik,hanem szöveg: 
+        pl:2/3/4/5/ert/4/6/ads/-12
+        ami szám, azt adja össze, szövegeket hagyja ki
+    url: osszead/2/3[/4/5/6/7]
+    method:GET
+kivonás
+    min 2 szám kivonasa
+    csak 1 szám: hiba: min 2 szám kell
+    ha sok szám: vonja ki mind
+    ha nem szám valamelyik,hanem szöveg: 
+        pl:2/3/4/5/ert/4/6/ads/-12
+        ami szám, azt vonja ki, szövegeket hagyja ki
+    url: kivon/2/3[/4/5/6/7]
+    method:GET
+szorzás
+    min 2 szám szorzása
+    csak 1 szám: hiba: min 2 szám kell
+    ha sok szám: szorozza össze mind
+    ha nem szám valamelyik,hanem szöveg: 
+        pl:2/3/4/5/ert/4/6/ads/-12
+        legyen hiba!
+    url: szoroz/2/3[/4/5/6/7]
+    method:GET
+osztás
+    min 2 szám hányadosa
+    csak 1 szám: hiba: min 2 szám kell
+    ha sok szám: balról jobbra osztás
+    ha nem szám valamelyik,hanem szöveg: 
+        pl:2/3/4/5/ert/4/6/ads/-12
+        legyen hiba!
+    ha 0: hiba: nullával való osztás
+    ha az első 0:return 0
+    url:oszt/2/3[/4/5/6/7]
+    method:GET
+*/
+
+phpinfo(32);
+
 $apiParts = explode("/", $_GET["path"]);
-// d($apiParts);
+
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    switch($apiParts[0])
-    {
-        case "osszead": break;
-        case "kivonas": break;
-        case "szorzas": break;
-        case "osztas": break;
-        default: "valami hiba";
+
+    switch ($apiParts[0]) {
+        case "osszead":
+            include_once "include/osszead.php";
+            osszead($apiParts);
+            echo osszead($apiParts);
+            break;
+        case "kivon":
+            break;
+        case "szoroz":
+            break;
+        case "oszt":
+            break;
+        default:
+            echo "Hiba: Érvénytelen művelet! (" . $apiParts[0] . ")";
+            break;
     }
-} 
-else {
-    echo "Nem GET kérést kaptunk";
+
+} else {
+    echo "Hibe, nem GET";
 }
 
+
+
+?>
--- 2025-2026/API/muveletek/include/osszead.php
+++ 2025-2026/API/muveletek/include/osszead.php
@@ -1,18 +1,10 @@
 <?php
-function osszead($szamok) {
-     
+include_once("szamkeres.php");
+function osszead($szamok){
+    // osszead/1/2/3/4/qwe/12
+    // ["osszead","1","2","3","4","qwe","12"]
+    // ami kell: [1,2,3,4,12]
+    $csakSzamok = szamKeres($szamok);
+    return array_sum($csakSzamok);
 }
-
-function szamkeres($szamok){
-    $vissza = [];
-
-    for($i=0;$i<count($szamok);$i++)
-    {
-        if(is_numeric($szamok[$i]))
-        {
-            $vissza[] = $szamok[$i];
-        }
-    }
-
-    return $vissza;
-}
+?>
