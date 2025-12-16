4423c5e83a5bc99e860b785ccc612be5006c74a0
100TMrMaTe <konyhasi.mate.21@ady-nagyatad.hu>
2025-12-16 10:33:09 +0100

    update

--- 2025-2026/PHP/mail/mailtest.php
+++
@@ -1,8 +0,0 @@
-<?php
-$msg = "First line of text\nSecond line of text";
-
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
-
-// send email
-mail("konyhasi.mate.21@ady-nagyatad.hu","My subject",$msg);
