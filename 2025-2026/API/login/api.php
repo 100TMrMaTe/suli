aeb025e1201ccabad411508a66840730386928af
100TMrMaTe <konyhasi.mate.21@ady-nagyatad.hu>
2025-12-18 14:55:21 +0100

    update

--- 2025-2026/API/login/api.php
+++ 2025-2026/API/login/api.php
@@ -8,30 +8,43 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $json = file_get_contents("php://input");
     $data = json_decode($json, true);
     //d($data);
-    if($apiParts[0] == "login")
-    {
-        $query = "SELECT id,password FROM users WHERE username ='".mysqli_real_escape_string($conn,$data["nev"])."'";
+    if ($apiParts[0] == "login") {
+        $query = "SELECT id,password FROM users WHERE username ='" . mysqli_real_escape_string($conn, $data["nev"]) . "'";
         $result = mysqli_query($conn, $query);
         $users = [];
-        while($row = mysqli_fetch_assoc($result))
-        {
+        while ($row = mysqli_fetch_assoc($result)) {
             $users[] = $row;
         }
 
-        if(sizeof($users)==1 && password_verify($data["password"],$users[0]["password"]))
-        {
+        if (sizeof($users) == 1 && password_verify($data["password"], $users[0]["password"])) {
             //echo "jo";
             $token = bin2hex(random_bytes(32));
             $expires = date("Y-m-d H:i:s", strtotime("+30 minutes"));
             //d($token);
             //d($expires);
 
-            $query = "INSERT INTO TOKEN (user_id,token,date) VALUES('".mysqli_real_escape_string($conn,$users[0]["id"])."','$token','$expires')";
+            $query = "INSERT INTO TOKEN (user_id,token,date) VALUES('" . mysqli_real_escape_string($conn, $users[0]["id"]) . "','$token','$expires')";
 
+            mysqli_query($conn, $query);
+
+            $json = [
+                "token" => $token,
+                "expires" => $expires,
+                "status" => "success"
+            ];
+        } else {
+            //echo "rossz";
+            $json = [
+                "status" => "error",
+                "message" => "Hibás felhasználónév vagy jelszó!"
+            ];
         }
-        else{
-            echo "rossz";
-        }
+        echo json_encode($json);
+    }
+} else if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if ($apiParts[0] == "logincheck") {
+        $header = getallheaders();
+        d($header);
     }
 }
 
--- 2025-2026/API/login/login.js
+++ 2025-2026/API/login/login.js
@@ -12,5 +12,27 @@ function login() {
         .then((x) => x.json())
         .then((valasz) => {
             console.log(valasz);
+            if (valasz.status == "success") {
+                localStorage.setItem("token", valasz.token);
+                localStorage.setItem("expires", valasz.expires);
+                document.getElementById("login").classList.add("d-none");
+                document.getElementById("oldal").classList.remove("d-none");
+            } else {
+                alert(valasz.message);
+            }
         });
+}
+function logincheck() {
+    
+    fetch("logincheck", {
+        method: "GET",
+        headers: {
+            "authorization": `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+        },
+    })
+    .then((x) => x.json())
+    .then((valasz) => {
+       console.log(valasz);
+    });
 }
