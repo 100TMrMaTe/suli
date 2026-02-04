882b403b509e7ea771a486c08ce28e4004b410be
100TMrMaTe <konyhasi.mate.21@ady-nagyatad.hu>
2026-02-04 13:19:46 +0100

    ok

---
+++ 2025-2026/LARAVEL/onallo1/app/Http/Controllers/Api/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    function index() {
+        return response()->json(['message' => 'API is working!', 'status' => 200]);
+    }
+}
---
+++ 2025-2026/LARAVEL/onallo1/routes/api.php
@@ -0,0 +1,7 @@
+<?php
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\TestController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/test', [TestController::class, 'index']);
+?>
