0493e35b63dcfd955ffe0f1a6f48b3018c4373dc
100TMrMaTe <konyhasi.mate.21@ady-nagyatad.hu>
2026-02-06 08:46:31 +0100

    ok

--- 2025-2026/C#/test/test/obj/Debug/net10.0/test.AssemblyInfo.cs
+++ 2025-2026/C#/test/test/obj/Debug/net10.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+882b403b509e7ea771a486c08ce28e4004b410be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b37c20b293231fa787fbfbbe47af46ef3ff63191")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
